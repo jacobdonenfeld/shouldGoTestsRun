42434255becd0029b9b7da103fe88e84997c4171
Jacob Donenfeld <25359163+jacobdonenfeld@users.noreply.github.com>
2022-06-15 21:45:54 +0000

    Add comments

--- gitChangeGoPath.go
+++ gitChangeGoPath.go
@@ -28,13 +28,14 @@ func main() {
 	baseImportName := flag.String("base-folder-name", "", "base folder name which imports are called from")
 	masterRepoBranchName := flag.String("master-repo-branch-name", "", "Name of the source branch to compare the current branch to")
 
-
+	// Parse input variables
 	flag.Parse()
 
 	var err error
 	var testDir string
 
 	if *testDirInput != "" {
+		// Convert relative path to absolute path from /
 		testDir, err = filepath.Abs(*testDirInput)
 		if err != nil {
 			fatal("Bad test directory path: %s\n", err)
@@ -44,7 +45,8 @@ func main() {
 	if err := gitChangeGoPath(testDir, *baseImportName, *masterRepoBranchName); err != nil {
 		fatal("Program Failed: %s", err)
 	}
-	os.Exit(0)
+	// If the progra
+	os.Exit(1)
 }
 
 func gitChangeGoPath(testDir string, baseImportName string, masterRepoBranchName string) error {
@@ -58,7 +60,7 @@ func gitChangeGoPath(testDir string, baseImportName string, masterRepoBranchName
 	// Get full list of imports here, so far just using initial imports
 	fullLocalImportList := initialFileImports
 
-
+	// Get list of files changed from current branch to master branch
 	gitDiffBytesOutput, err := exec.Command("git", "diff", "--name-only", masterRepoBranchName).Output()
 	if err != nil {
 		fatal("Issue Running git diff: %s", err)
@@ -66,26 +68,35 @@ func gitChangeGoPath(testDir string, baseImportName string, masterRepoBranchName
 	gitDiffFiles := strings.Split(string(gitDiffBytesOutput), "\n")
 	var gitDiffFilesWithBaseAppended []string
 	for _, gitDiffFile := range gitDiffFiles {
+		// Path from root of project, where go.mod would exist
 		gitDiffFilesWithBaseAppended = append(gitDiffFilesWithBaseAppended, filepath.Join(baseImportName, gitDiffFile))
 	}
 	fmt.Println(fullLocalImportList)
 
 	importMap := stringSliceToMap(fullLocalImportList)
 	
+	// For all the files changed in the branch, path from go.mod root
 	for _, diffFileName := range gitDiffFilesWithBaseAppended {
+		// If a file changed is imported by a test in the test directory specified
 		if importMap[diffFileName]{
+			// Exit, because bash: if [gitchangegopath] then exit 0; else run test
 			os.Exit(1)
+			
 		}
 	}
 
 	return nil
 }
 
+// Return a list of import names that all go files in the provided directory import.
+// if filterTestOnly = true, only considers files that end in test.go
 func getDirectoryImports(directory string, filterTestsOnly bool, baseImportFolder string) ([]string, error) {
 	if len(directory) == 0 {
 		fatal("No directory Provided\n")
 		return []string{}, nil
 	}
+	
+	// Collect files in provided directory
 	files, err := ioutil.ReadDir(directory)
 	if err != nil {
 		fatal("Could not read files in test directory: %s\n", err)
@@ -108,6 +119,7 @@ func getDirectoryImports(directory string, filterTestsOnly bool, baseImportFolde
 			continue
 		}
 		fullImportPath = filepath.Join(directory, file.Name()) // Works, filepath from root
+		// Get list of packages that the go file imports
 		inportNameIntermediate, err = getImportsFromFile(fullImportPath, baseImportFolder)
 		fmt.Println(inportNameIntermediate)  // Not working
 		if err != nil {
@@ -119,8 +131,8 @@ func getDirectoryImports(directory string, filterTestsOnly bool, baseImportFolde
 	return unique(importNames), nil
 }
 
-
-// Do this by packages instead
+// Get list of packages that the go file imports
+// Only considers local packages, by checking if the import path has the root folder that the go.mod file lives in
 func getImportsFromFile(fullFilePath string, baseImportFolder string) ([]string, error) {
 	// represents a set of source files and which we need for the parser
 	fset := token.NewFileSet()
